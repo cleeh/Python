aebe58e5551719c95dfe50f1774f7389e9536797
rockatoo <youlia73@naver.com>
2018-08-08 22:43:06 +0900

    Python Study: Comparison Operator User

---
+++ operator.py
@@ -0,0 +1,7 @@
+compA = 20
+compB = 30
+
+print('compA =',compA,'compB =',compB)
+print('compA > compB:',compA > compB)
+print('compA < compB:',compA < compB)
+print('compA == compB:',compA == compB)
