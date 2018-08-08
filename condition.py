a104b2d949f480b29e76df52732116caa7dfe011
rockatoo <youlia73@naver.com>
2018-08-08 23:16:41 +0900

    Python Study: Condition Branch 'else' added

--- condition.py
+++ condition.py
@@ -1,7 +1,10 @@
 congraturationText = 'Congraturation! You have score higher than 90!'
+encourageText = "It's okay. There is next chance :)"
 congraturationScore = 90
-getScore = 95
+getScore = 40
 
 print('Test Score:',getScore)
 if getScore >= congraturationScore:
     print(congraturationText)
+else:
+    print(encourageText)
