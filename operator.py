98086fea7840c5514e427e21cc233cac9ffdb397
rockatoo <youlia73@naver.com>
2018-08-08 22:47:09 +0900

    Python Study: Using Minus(-) Operator

--- operator.py
+++ operator.py
@@ -5,3 +5,9 @@ print('compA =',compA,'compB =',compB)
 print('compA > compB:',compA > compB)
 print('compA < compB:',compA < compB)
 print('compA == compB:',compA == compB)
+print('')
+
+print('1204 - 48 =', 1204-48)
+minusA = 1204
+minusB = 48
+print('minusA(1204) - minusB(48) =', minusA - minusB)
