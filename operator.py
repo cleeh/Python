cc5bfbd098a725f53509d9deb4e9ff7de999cf93
rockatoo <youlia73@naver.com>
2018-08-08 23:01:31 +0900

    Python Study: Power Operator and Logical Operator Added

--- operator.py
+++ operator.py
@@ -17,3 +17,16 @@ Cake = 15000
 Pot = 800
 print('Soboru 17, Cake 3, Pot 8 are sold. then, how much income do you have?')
 print('Answer: Soboru * 17 + Cake * 3 + Pot * 8 =', Soboru*17 + Cake*3 + Pot*8)
+print('')
+
+print(2**3)
+print('')
+
+trueV = True
+falseV = False
+print('not trueV:',not trueV)
+print('not falseV:',not falseV)
+print('trueV and falseV',trueV and falseV)
+print('trueV or falseV:',trueV or falseV)
+print('trueV xor falseV:',trueV ^ falseV)
+print('')
