b21a333122f3ba22061def197e572229357642c8
rockatoo <youlia73@naver.com>
2018-08-08 22:50:57 +0900

    Python Study: Calculate Income

--- operator.py
+++ operator.py
@@ -1,6 +1,5 @@
 compA = 20
 compB = 30
-
 print('compA =',compA,'compB =',compB)
 print('compA > compB:',compA > compB)
 print('compA < compB:',compA < compB)
@@ -11,3 +10,10 @@ print('1204 - 48 =', 1204-48)
 minusA = 1204
 minusB = 48
 print('minusA(1204) - minusB(48) =', minusA - minusB)
+print('')
+
+Soboru = 1000
+Cake = 15000
+Pot = 800
+print('Soboru 17, Cake 3, Pot 8 are sold. then, how much income do you have?')
+print('Answer: Soboru * 17 + Cake * 3 + Pot * 8 =', Soboru*17 + Cake*3 + Pot*8)
