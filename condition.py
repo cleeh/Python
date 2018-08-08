6cb3ea9b82eee8f38df64dceeac4a0d15f742cba
rockatoo <youlia73@naver.com>
2018-08-08 23:13:45 +0900

    Python Study: First Condition Branch

---
+++ condition.py
@@ -0,0 +1,7 @@
+congraturationText = 'Congraturation! You have score higher than 90!'
+congraturationScore = 90
+getScore = 95
+
+print('Test Score:',getScore)
+if getScore >= congraturationScore:
+    print(congraturationText)
