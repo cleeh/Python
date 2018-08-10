f8eb052d27e5c0dc3441afecf9455bc5b38d1ced
rockatoo <youlia73@naver.com>
2018-08-10 19:29:38 +0900

    Python Study: Funciton Added

--- loop.py
+++ loop.py
@@ -14,3 +14,17 @@ for a in range(1, 10):
     print(a,'Stage')
     for b in range(1, 10):
         print(a,'*',b,'=',a*b)
+print('')
+
+def plus9(a):
+    return a + 9
+print('plus9(10):',plus9(10))
+print('')
+
+
+def Add(a,b):
+    return a + b
+print('Add(10, 20):', Add(10, 20))
+print('Add(3.14, 6.28):', Add(3.14, 6.28))
+print('Add(1 + 2j, 3 + 8j):', Add(1 + 2j, 3 + 8j))
+print('Add("Dog", " Wolrd"):',	 Add("Dog", " World"))
