bf895c28295a9d33f1e0f411fc6ddbd6a8fb0e44
rockatoo <youlia73@naver.com>
2018-08-10 23:27:07 +0900

    Python Study: String Operator

--- type.py
+++ type.py
@@ -8,17 +8,20 @@ print(isinstance(a, int))
 print(isinstance(b, int))
 print(isinstance(c, int))
 print(isinstance(d, int))
+print('')
 
 print("int('100'):",int('100'))
 print("a:",a)
 print("bin(a):",bin(a))
 print("oct(a):",oct(a))
 print("hex(a):",hex(a))
+print('')
 
 a=int('19', 16)
 print("int('19',16):",a)
 b=int('32', 8)
 print("int('32',8):",b)
+print('')
 
 class calculator:
     def mul(x, y):
@@ -36,3 +39,36 @@ class calculator:
 print(calculator.mul(10, 20))
 print(calculator.add(10, 20))
 print(calculator.total(10))
+print('')
+
+stringa = 'abcdefghi'
+print('stringa:',stringa)
+print('stringa[0]:',stringa[0])
+print('stringa[5]:',stringa[5])
+print('stringa[8]:',stringa[8])
+print('stringa[-1]:',stringa[-1])
+print('stringa[0:3]:',stringa[0:3])
+print('stringa[1:3]:',stringa[1:3])
+print('stringa[0:8]',stringa[0:8])
+print('stringa[:-1]:',stringa[:-1])
+print('')
+
+stringb = 'ice'
+stringc = ' '
+stringd = 'cream'
+print('stringb + stringc + stringd:',stringb + stringc + stringd)
+print('')
+
+print('stringb * 5:',stringb * 5)
+print('')
+
+print('"eam" in stringd:',"eam" in stringd)
+print('"ice" in stringd:',"ice" in stringd)
+print('')
+
+twostringa='''fighting
+today'''
+twostringb='''fighting \
+today'''
+print('twostringa:',twostringa)
+print('twostringb:',twostringb)
