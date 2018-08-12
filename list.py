5dac81e9b7d15291012c34a77d99312a8f4f1331
rockatoo <youlia73@naver.com>
2018-08-12 22:25:14 +0900

    List: Value Changing

--- list.py
+++ list.py
@@ -5,3 +5,8 @@ print(list[2:4])
 print(list + [5,1,"Newb","log"])
 print(list * 2)
 print(len(list))
+print('')
+
+print('Original List:',list)
+list[2] = list[2] + 20
+print('list[2] = list[2] + 20:',list[2])
