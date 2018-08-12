977383dbc32e4cc5203875798154ef85b065689b
rockatoo <youlia73@naver.com>
2018-08-12 23:27:10 +0900

    Dictionary: Another How to make dictionary

--- dictionary.py
+++ dictionary.py
@@ -4,3 +4,13 @@ print('volume[\'goodday\']:',volume['goodday'])
 print('len(volume):',len(volume))
 del volume['hanrasan']
 print('del volume[\'hanrasan\']:',volume)
+print('')
+
+keys = ['truedew','goodday','hanrasan']
+values = [360,360,375]
+temp = zip(keys, values)
+tuple = dict(temp)
+print('keys:',keys)
+print('values:',values)
+print('temp = zip(keys, values):\n',temp)
+print('tuple = dict(temp):\n',tuple)
