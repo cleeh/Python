6145b77cd5ccf2ad5e3e666dc7b91ea156859bc4
rockatoo <youlia73@naver.com>
2018-08-13 18:27:43 +0900

    Stack

--- multi-use.py
+++ multi-use.py
@@ -11,3 +11,11 @@ while True:
 for x in range(len(queue)):
     print("Sequence:",x + 1,"Data:",queue[x])
 print('')
+
+stack = []
+while True:
+    data = input('input data = ')
+    if(data == 'q'): break
+    stack.append(data)
+for x in range(len(stack)):
+    print("Sequence:",x + 1,"Data:",stack[len(stack)-x-1])
