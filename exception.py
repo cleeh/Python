3149ad1896ae5233b247a97db5a3d2000782356a
rockatoo <youlia73@naver.com>
2018-08-13 22:23:24 +0900

    Exception: TypeError

--- exception.py
+++ exception.py
@@ -2,3 +2,8 @@ try:
     print(100/0)
 except ZeroDivisionError:
     print('Divided by 0')
+    
+try:
+    print("String" + 100)
+except TypeError:
+    print('Two object type is not matchec each other')
