fc4dcfb0f44637885b2b2820c1306345ac4c8627
rockatoo <youlia73@naver.com>
2018-08-13 18:27:20 +0900

    Queue

--- multi-use.py
+++ multi-use.py
@@ -2,3 +2,12 @@ stage = int(input('What stage do you want? '))
 for x in range(1,10):
     print(stage,'*',x,'=',int(stage * x))
 print('')
+
+queue = []
+while True:
+    data = input('input data = ')
+    if(data == 'q'): break
+    queue.append(data)
+for x in range(len(queue)):
+    print("Sequence:",x + 1,"Data:",queue[x])
+print('')
