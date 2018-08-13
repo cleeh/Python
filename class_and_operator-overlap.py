f5d270abc705c89ce3c4bc4f969a727ca89a0546
rockatoo <youlia73@naver.com>
2018-08-13 19:01:54 +0900

    Class and Operator-Overlap: Bound Method Call

--- class_and_operator-overlap.py
+++ class_and_operator-overlap.py
@@ -3,8 +3,18 @@ class GeneralMethod:
         self.value = v
     def get(self):
         return self.value
+
+# Unbound Method Call
 classInstance = GeneralMethod()
 print('GeneralMethod.set:',GeneralMethod.set)
 print('classInstance.set:',classInstance.set)
 GeneralMethod.set(classInstance, 'Unbound Method Call')
 print(GeneralMethod.get(classInstance))
+print('')
+
+# Bound Method Call using instance object
+classInstance2 = GeneralMethod()
+print('classInstance2:',classInstance2)
+print('classInstance2.set:',classInstance2.set)
+classInstance2.set("Bound Method Call")
+print('classInstance2.get():',classInstance2.get())
