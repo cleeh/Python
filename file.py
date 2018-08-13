f36f531a263fc8e3934e7987be561896689a4010
rockatoo <youlia73@naver.com>
2018-08-13 23:15:34 +0900

    FileIO: File Object Methods and Attribute
    Signed-off-by: rockatoo <youlia73@naver.com>

--- file.py
+++ file.py
@@ -27,3 +27,22 @@ with open('fileIO.txt') as files:
 with open('fileIO.txt') as files:
     print('files.readlines():',files.readlines())
     files.close()
+
+files = open('fileIO.txt','w+')
+print('files.tell():',files.tell()) # Return Current File Pointer Location
+print('files.seek(3,0):',files.seek(10)) # Set File Pointer Location
+print('files.tell():',files.tell())
+print('files.read():\n' + files.read())
+print('files.read():\n' + files.read())
+print('files.fileno():',files.fileno()) # Return File Number
+files.flush() # Buffer Flush
+print('files.isatty():',files.isatty()) # If 'files' is tty, return 1 or TRUE. If not, return 0 or FALSE
+files.seek(5)
+print('files.truncate():',files.truncate())
+files.seek(0)
+print('files.read():\n' + files.read())
+print('files.mode:',files.mode)
+print('files.name:',files.name)
+print('[Before files.close()] files.closed:',files.closed)
+files.close()
+print('[After files.close()] files.closed:',files.closed)
