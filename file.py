cb629cb8598bc8c914ae15e4d5df628f64996c92
rockatoo <youlia73@naver.com>
2018-08-13 22:47:40 +0900

    FileIO: File Read and writelines(<List>)
    Signed-off-by: rockatoo <youlia73@naver.com>

--- file.py
+++ file.py
@@ -9,3 +9,11 @@ files.close()
 
 with open('fileIO.txt', 'w', encoding = 'utf-8') as files:
     files.write("한글 만세!\n")
+    files.close()
+
+lines = ['line #1\n', 'line #2\n', 'line #3']
+with open('fileIO.txt','w') as files:
+    files.writelines(lines)
+with open('fileIO.txt') as files:
+    print(files.read())
+    files.close()
